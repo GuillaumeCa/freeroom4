b45ab181b0e4bfa2d00893b898cca3e30e348556
GuillaumeCa <guillaume.carre@isep.fr>
2016-11-07 23:25:42 +0100

    init

---
+++ src/components/RoomsList/RoomsList.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import './RoomsList.css';
+
+const list = [
+  {
+    id: 'N43',
+    floor: 4,
+    events: [
+      {
+        title: 'N43 - test1',
+        start: new Date().getTime() + 10000,
+        end: new Date().getTime() + 12000
+      },
+      {
+        title: 'N43 - test2',
+        start: new Date().getTime() + 20000,
+        end: new Date().getTime() + 22000
+      },
+    ]
+  },
+  {
+    id: 'N46',
+    floor: 4,
+    events: [
+      {
+        title: 'N44 - test1',
+        start: new Date().getTime() - 1000,
+        end: new Date().getTime() + 12000
+      },
+      {
+        title: 'N44 - test2',
+        start: new Date().getTime() + 13000,
+        end: new Date().getTime() + 22000
+      },
+    ]
+  }
+]
+
+function RoomTime(props) {
+  const getTime = function (timestamp) {
+    return new Date(timestamp).toLocaleTimeString()
+  }
+  return <div>{props.title} - {getTime(props.start)} - {getTime(props.end)}</div>
+}
+
+function Room(props) {
+  const current = props.current ? <div>CURRENT: <RoomTime {...props.current} /></div> : ''
+  const next = props.next ? <div>NEXT: <RoomTime {...props.next} /></div> : ''
+  return (
+    <div>
+      <div>{props.id} - {props.status === 'free' ? 'disponible' : 'non disponible'}</div>
+      {current}
+      {next}
+      <ul>
+        { props.events.map((item, index) => <li key={index}><RoomTime {...item} /></li>)}
+      </ul>
+    </div>
+  )
+}
+
+export class RoomsList extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { list }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.checkRoomOccupation()
+    }, 1000)
+  }
+
+  checkRoomOccupation() {
+    const date = new Date()
+    const update = this.state.list.map(room => {
+      room.current = room.events.filter(e => e.start < date && date < e.end )[0]
+      room.next = room.events.filter(e => date < e.start)[0]
+      return room
+    })
+
+    this.setState({
+      ...this.state,
+      list: update
+    })
+  }
+
+  render() {
+    const list = this.state.list
+      .map((item, index) => (
+        <Room
+          key={index}
+          id={item.id}
+          status={item.status}
+          events={item.events}
+          current={item.current}
+          next={item.next} />
+      ))
+    return (
+      <div>
+        { list }
+      </div>
+    )
+  }
+}

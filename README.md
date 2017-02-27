34ab0f3d5a0b239806679b3aa7714f228e0f4e62
GuillaumeCa <guillaume.carre@isep.fr>
2017-02-28 00:13:35 +0100

    refactor files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Freeroom 4
-New version of freeroom built with state of the art features using React. Backend incoming.
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import room from './roomReducer';
-
-export default combineReducers({
-  room
-})

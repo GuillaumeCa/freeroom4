b1c969c4c94fdb59cd0da7490f5cb84a67b29cd5
GuillaumeCa <guillaume.carre@isep.fr>
2018-02-07 18:13:52 +0100

    fix update window

--- api/app/services.go
+++ api/app/services.go
@@ -44,10 +44,13 @@ func (a *App) updateCalendars(conf roomConfObj, b string) {
 		events := getEvents(b, rid, r.URLID)
 		a.Model.insertRoom(rid, b)
 
+		now := time.Now()
+		currentDate := now.Truncate(24 * time.Hour)
+
 		eventsToAdd := []Event{}
 		for _, e := range events {
 			timeOffset := time.Now().Truncate(24*time.Hour).AddDate(0, 0, 7)
-			isTimeInBounds := e.Start.After(time.Now()) && e.Start.Before(timeOffset)
+			isTimeInBounds := e.Start.After(currentDate) && e.Start.Before(timeOffset)
 			if e.Summary != "Férié" && isTimeInBounds {
 				ev := Event{
 					Name:        e.Summary,
